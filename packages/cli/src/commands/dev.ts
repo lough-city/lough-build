941a629e780b062cd63b6a4e3d6cc589e648b7d4
City <city@nanzc.com>
2022-12-05 13:48:40 +0800

    fix: lyrical js import

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -1,14 +1,15 @@
 import chokidar from 'chokidar';
-import { createDebounceInterval } from '@lyrical/js';
 import build from './build';
 import { startSpinner } from '../utils/spinner';
-
-const buildDebounce = createDebounceInterval(build.action, { interval: 500 });
 
 const action = async () => {
   const rootPath = process.cwd();
 
   startSpinner('dev watch: ' + rootPath);
+
+  const lyricalJs = await import('@lyrical/js');
+
+  const buildDebounce = lyricalJs.createDebounceInterval(build.action, { interval: 500 });
 
   buildDebounce();
 
